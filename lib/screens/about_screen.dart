89912ddac326a24787d63b7c80aa1fdbbf837464
Vladyslav Dubenchuk <253847@p.lodz.pl>
2025-10-29 11:12:10 +0100

    забыл сука версию дописать в ебаут меню Version 1.3.0

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -56,7 +56,7 @@ class AboutScreen extends StatelessWidget {
             ),
             const SizedBox(height: 5),
             Text(
-              'Version 1.1.0',
+              'Version 1.3.0',
               style: TextStyle(
                 fontSize: 14,
                 color: Colors.grey.shade600,
